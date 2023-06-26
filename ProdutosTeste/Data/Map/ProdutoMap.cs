ef9aef665b1079c70d9c0ebec8569ad140319dca
EduardGuilherme <eduguilherme48@gmail.com>
2023-06-26 13:01:24 -0300

    Update map

--- ProdutosTeste/Data/Map/ProdutoMap.cs
+++ ProdutosTeste/Data/Map/ProdutoMap.cs
@@ -11,11 +11,11 @@ namespace TesteProdutos.Data.Map
             builder.HasKey(x => x.ProdutoId);
             builder.Property(x => x.Nome).IsRequired();
             builder.Property(x => x.Categoria).IsRequired();
-            /*builder.HasMany(p => p.ItensPedido)
+            builder.HasMany(p => p.ItensPedido)
                 .WithOne(ip => ip.Produto)
                 .HasForeignKey(ip => ip.ProdutoId)
                 .IsRequired()
-                .OnDelete(DeleteBehavior.Restrict);*/
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
